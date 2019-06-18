23738f46c8856af52df94b1f41bb77c94d08da77
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2019-06-18 23:18:28 +0300

    minor refactoring

--- kts_cli/examples.py
+++ kts_cli/examples.py
@@ -1,5 +1,6 @@
 import os
 import time
+import shutil
 
 VALID_EXAMPLES = ['titanic']
 
@@ -9,5 +10,7 @@ def download_example(args):
     if os.path.exists(name):
         raise OSError(f'Path ./{name} already exists.')
     download_name = ".kts_examples_" + str(int(time.time()))
-    os.system(f"git clone https://github.com/konodyuk/kts-examples {download_name} &&"
-              f" cp -rf {download_name}/{name} . && rm -rf {download_name} && cd {name} && ./init.sh")
+    os.system(f"git clone https://github.com/konodyuk/kts-examples {download_name}")
+    shutil.move(f"{download_name}/{name}", f"{name}")
+    os.system(f"""cd {name} && echo "n\ny\ny" | kts init""")
+    shutil.rmtree(f"{download_name}")
